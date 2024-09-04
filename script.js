9884c2bb05ce0d976d1361b3dc2e44612978e539
RK-ShadowDragon <66867955+RK-ShadowDragon@users.noreply.github.com>
2024-09-04 11:47:38 +0530

    awakened runes, renew

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+function toggleContent(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
